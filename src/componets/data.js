ea04521ef6e78f5b7d7f039e941faa52f0f66827
DenisFadeev94 <denis.ffadeev@gmail.comm>
2025-06-14 12:00:10 +0300

    add data.js and npm sass

---
+++ src/componets/data.js
@@ -0,0 +1,90 @@
+export const headerNav = [
+  {
+    id: 1,
+    title: 'Навчальний транспорт'
+  },
+  {
+    id: 2,
+    title: 'Навчальний кабінет'
+  },
+  {
+    id: 3,
+    title: 'Адреса'
+  },
+  {
+    id: 4,
+    title: 'Відгуки'
+  }
+];
+
+export const ready = [
+  {
+    id: 1,
+    title: 'Денні та вечірні групи',
+    description: 'A JavaScript library for building user interfaces',
+    image: 'https://reactjs.org/logo-og.png',
+    link: 'https://reactjs.org/'
+  },
+  {
+    id: 2,
+    title: 'Додаткові уроки з водіння автомобіля',
+    description: 'Next Generation Frontend Tooling',
+    image: 'https://vitejs.dev/logo.svg',
+    link: 'https://vitejs.dev/'
+  },
+  {
+    id: 3,
+    title: 'Якісна теоретична та практична підготовка',
+    description: 'A programming language for the web',
+    image: 'https://www.javascript.com/images/logo.svg',
+    link: 'https://www.javascript.com/'
+  },
+  {
+    id: 4,
+    title:
+      'Оплата частинами за навчання в період підготовки. Детальна інформація про вартість навчання за вказаними номерами телефону',
+    description: 'A programming language for the web',
+    image: 'https://www.javascript.com/images/logo.svg',
+    link: 'https://www.javascript.com/'
+  }
+];
+
+export const reviews = [
+  {
+    id: 1,
+    description: 'A JavaScript library for building user interfaces'
+  },
+  {
+    id: 2,
+    description: 'A JavaScript library for building user interfaces'
+  }
+];
+
+export const documents = [
+  {
+    id: 1,
+    description: `Копія паспорту та прописка,
+                  або витяг чи довідка про
+                  реєстрацію місця проживання
+                  (1, 2 стр. та прописка)`,
+    image: 'https://www.javascript.com/images/logo.svg'
+  },
+  {
+    id: 2,
+    description: `Копія
+                  ідентифікаційного номеру`,
+    image: 'https://www.javascript.com/images/logo.svg'
+  },
+  {
+    id: 3,
+    description: `Медична довідка
+                  (форма № 083/0)`,
+    image: 'https://www.javascript.com/images/logo.svg'
+  },
+  {
+    id: 4,
+    description: `Кольорові фотокартки
+                  3 шт. розміром 3.5 х 4.5 см`,
+    image: 'https://www.javascript.com/images/logo.svg'
+  }
+];
